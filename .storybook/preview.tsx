d73f84d89c05f86161a4957a50526f5012870b67
Bardia Jedi <bardiajedi@gmail.com>
2023-05-20 15:41:04 +0200

    chore(storybook): renamed preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,23 +1,20 @@
+import React from "react";
 import type { Preview } from "@storybook/react";
 import { withThemeFromJSXProvider } from "@storybook/addon-styling";
 import { createGlobalStyle } from "styled-components";
-
-const GlobalStyles = createGlobalStyle`
-  body {
-    font-family: "-apple-system","BlinkMacSystemFont","Segoe UI","Roboto","Oxygen","Ubuntu","Cantarell","Fira Sans","Droid Sans","Helvetica Neue","sans-serif";
-  }
-`;
+import { ThemeWrapper } from "../src";
 
 export const decorators = [
-  withThemeFromJSXProvider({
-    // {GlobalStyles}, // Adds your GlobalStyle component to all stories
-    GlobalStyles,
-  }),
+  (Story) => (
+    <ThemeWrapper>
+      <Story />
+    </ThemeWrapper>
+  ),
 ];
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
-
+    // decorators: [],
     controls: {
       matchers: {
         color: /(background|color)$/i,
