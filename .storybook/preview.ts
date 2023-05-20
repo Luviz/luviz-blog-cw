5fe8999413a74385a2dfe9b649929224c556d886
Bardia Jedi <bardiajedi@gmail.com>
2023-05-20 11:39:26 +0200

    feat: added global font family

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,23 @@
 import type { Preview } from "@storybook/react";
+import { withThemeFromJSXProvider } from "@storybook/addon-styling";
+import { createGlobalStyle } from "styled-components";
 
+const GlobalStyles = createGlobalStyle`
+  body {
+    font-family: "-apple-system","BlinkMacSystemFont","Segoe UI","Roboto","Oxygen","Ubuntu","Cantarell","Fira Sans","Droid Sans","Helvetica Neue","sans-serif";
+  }
+`;
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    // {GlobalStyles}, // Adds your GlobalStyle component to all stories
+    GlobalStyles,
+  }),
+];
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
+
     controls: {
       matchers: {
         color: /(background|color)$/i,
