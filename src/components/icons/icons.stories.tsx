615cc7340652f4ef8e0ead546ba2e5e954c659eb
Bardia Jedi <bardiajedi@gmail.com>
2023-05-26 20:41:38 +0200

    feat(icon): added linkedin icon

--- src/components/icons/icons.stories.tsx
+++ src/components/icons/icons.stories.tsx
@@ -1,14 +1,34 @@
 import React from "react";
 import { Meta, StoryFn } from "@storybook/react";
 import { GitHub } from "./github";
+import { LinkedIn } from "./linkedin";
+import { svgProps } from "./svg-props";
 
 export default {
   title: "Blog/Icons",
   component: GitHub,
 } as Meta<typeof GitHub>;
 
-export const githubIcon: StoryFn<typeof GitHub> = (args) => (
+export const icons: StoryFn<svgProps> = (args) => {
+  return (
+    <div style={{ display: "flex", gap: "0.5rem" }}>
+      <GitHub {...args} />
+      <LinkedIn {...args} />
+    </div>
+  );
+};
+icons.storyName = "Icons List";
+
+export const github: StoryFn<svgProps> = (args) => (
   <div>
     <GitHub {...args} />
   </div>
 );
+github.storyName = "GitHub";
+
+export const linkedIn: StoryFn<svgProps> = (args) => (
+  <div>
+    <LinkedIn {...args} />
+  </div>
+);
+linkedIn.storyName = "LinkedIn";
