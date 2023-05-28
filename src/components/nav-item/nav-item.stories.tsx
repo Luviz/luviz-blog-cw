4d1228a8c552b25d38e8f85e0018478028eb59b5
Bardia Jedi <bardiajedi@gmail.com>
2023-05-28 21:29:48 +0200

    feat(nav item): added button and animation

--- src/components/nav-item/nav-item.stories.tsx
+++ src/components/nav-item/nav-item.stories.tsx
@@ -1,16 +1,118 @@
 import React from "react";
 import { Meta, StoryFn } from "@storybook/react";
-import { NavItem } from "./nav-item";
+import { NavItem, NavItemSize } from "./nav-item";
+import { GitHub, LinkedIn, Twitter, BlogIcon, svgProps } from "../icons";
+
+const Icons: Record<string, React.FC<svgProps> | undefined> = {
+  none: undefined,
+  GitHub: GitHub,
+  LinkedIn: LinkedIn,
+  Twitter: Twitter,
+  BlogIcon: BlogIcon,
+};
+
+type storyProps = typeof NavItem & {
+  iconValue: string;
+};
 
 export default {
   title: "Blog/NavItem",
   component: NavItem,
-} as Meta<typeof NavItem>;
+  argTypes: {
+    iconValue: {
+      options: Object.keys(Icons),
+      control: { type: "select" },
+      default: "none",
+    },
+  },
+} as Meta<storyProps>;
 
-const template: StoryFn<typeof NavItem> = (args) => <NavItem {...args} />;
+const template: StoryFn<storyProps> = (args) => {
+  return <NavItem {...args} icon={Icons[args.iconValue]} />;
+};
 
 export const navItemHref = template.bind({});
 navItemHref.args = {
   label: "Text",
-  href: ""
+  iconValue: 'GitHub',
+  href: "",
+};
+
+export const topNavItems:StoryFn<storyProps> = ({size}) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "1rem",
+        backgroundColor: "#4d0000",
+        padding: ".5rem",
+        justifyContent: 'flex-end'
+      }}
+    >
+      <NavItem
+        icon={BlogIcon}
+        label="Blog"
+        size={size}
+        iconValue={""}
+      />
+      <NavItem
+        icon={Twitter}
+        label="Twitter"
+        size={size}
+        iconValue={""}
+      />
+      <NavItem
+        icon={LinkedIn}
+        label="LinkedIn"
+        size={size}
+        iconValue={""}
+      />
+      <NavItem
+        icon={GitHub}
+        label="Github"
+        size={size}
+        iconValue={""}
+      />
+    </div>
+  );
+};
+
+
+export const topNavItemsVert:StoryFn<storyProps> = ({size}) => {
+  return (
+    <div
+      style={{
+        width: 'fit-content',
+        gap: "1rem",
+        backgroundColor: "#4d0000",
+        padding: ".5rem",
+        justifyContent: 'flex-end'
+      }}
+    >
+      <NavItem
+        icon={BlogIcon}
+        label="Blog"
+        size={size}
+        iconValue={""}
+      />
+      <NavItem
+        icon={Twitter}
+        label="Twitter"
+        size={size}
+        iconValue={""}
+      />
+      <NavItem
+        icon={LinkedIn}
+        label="LinkedIn"
+        size={size}
+        iconValue={""}
+      />
+      <NavItem
+        icon={GitHub}
+        label="Github"
+        size={size}
+        iconValue={""}
+      />
+    </div>
+  );
 };
--- src/components/nav-item/nav-item.tsx
+++ src/components/nav-item/nav-item.tsx
@@ -1,16 +1,41 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import "./style.css";
+import { svgProps } from "../icons";
+
+export enum NavItemSize {
+  small = "small",
+  large = "large",
+}
 
 export type NavItemProps = {
+  iconValue: any;
+  label: string;
+  icon?: FC<svgProps>;
   onClick?: () => void;
   href?: string;
-  label: string;
+  size?: NavItemSize;
 };
 
-export const NavItem: FC<NavItemProps> = ({ label, href, onClick }) => {
+export const NavItem: FC<NavItemProps> = ({
+  label,
+  href,
+  onClick,
+  icon: SVGIcon,
+  size = NavItemSize.large,
+}) => {
+  const renderText = size === NavItemSize.large || !!!SVGIcon;
   return (
-    <a className="nav-item-container" onClick={onClick} href={href}>
-      {label}
+    <a
+      className="nav-item-anchor "
+      onClick={onClick}
+      href={href}
+    >
+      <div className="nav-item-container">
+        {SVGIcon && <SVGIcon fill="#ccc" height="1.5rem" width="1.5rem" />}
+        <span className="nav-item-text" data-hidden={!renderText}>
+          {label}
+        </span>
+      </div>
     </a>
   );
 };
